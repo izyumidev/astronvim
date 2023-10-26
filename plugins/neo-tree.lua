6492994937810c1a4bd32e2acbba103d0a3a76d2
Yumi Izumi <me@yumiizumi.com>
2023-10-26 13:22:21 -0400

    Add new plugins for community, copilot, neo-tree, nvim-cmp, and presence.

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,12 +1,10 @@
 return {
-{
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-      },
-    },
-  },
-}
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true
+            }
+        }
+    }
 }
